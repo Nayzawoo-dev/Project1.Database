880f31c3ad3affe0fd4bf41abb51e6409bb7781a
Nayzawoo-dev <nayzawoo.ace@gmail.com>
2025-07-08 13:46:28 +0630

    feat: addMvc2

---
+++ MVC2/Controllers/WalletController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC2.Controllers
+{
+    public class WalletController : Controller
+    {
+        [ActionName("Index")]
+        public IActionResult WalletList()
+        {
+            return View("WalletList");
+        }
+    }
+}
