6c81e3e668289895035d37d66b366665b8625820
Nayzawoo-dev <nayzawoo.ace@gmail.com>
2025-07-28 22:33:50 +0630

    feat: addajax

--- MVC2/Controllers/WalletController.cs
+++ MVC2/Controllers/WalletController.cs
@@ -1,13 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Data;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace MVC2.Controllers
 {
     public class WalletController : Controller
     {
+        private readonly SqlConnectionStringBuilder _connection;
+
+        public WalletController(IConfiguration configuration)
+        {
+            _connection = new SqlConnectionStringBuilder(configuration.GetConnectionString("DbConnection"));
+        }
+        
         [ActionName("Index")]
-        public IActionResult WalletList()
+        public IActionResult WalletIndex()
         {
             return View("WalletList");
         }
+
+        [HttpPost]
+        [ActionName("Index")]
+        public async Task<IActionResult> WalletList()
+        {
+            try
+            {
+                using IDbConnection connection = new SqlConnection(_connection.ConnectionString);
+                string query = "select * from Tbl_Wallet";
+                connection.Open();
+                var res = await connection.QueryAsync<WalletModel>(query);
+                var result = res.ToList();
+                return Json(new { isSuccess = "true", message = "Successful", data = result });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { isSuccess = "false", message = ex.ToString() });
+            }
+        }
+    }
+
+    public class WalletModel
+    {
+        public int WalletId { get; set; } 
+        public string WalletUserName { get; set; }
+        public string FullName { get; set; }
+        public string MobileNo { get; set; }
+        public decimal Balance { get; set; }
     }
 }
